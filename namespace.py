98307019859b17a40d5188868f177b8e46a716c1
Tomislav Peharda <->
2022-03-19 21:20:20 +0100

    Add namespace methods

--- namespace.py
+++ namespace.py
@@ -6,7 +6,19 @@ class APiNamespace( dict ):
     environment and holon identifiers.
     '''
     def __init__( self, *args, **kwargs ):
-        self[ 'agents' ] = {}
-        self[ 'channels' ] = {}
-        self[ 'environment' ] = {}
-        self[ 'holons' ] = {}
+        self[ 'agents' ] = []
+        self[ 'channels' ] = []
+        self[ 'environment' ] = []
+        self[ 'holons' ] = []
+
+    def add_agent ( self, agent ):
+        self[ 'agents' ].append( agent )    
+        
+    def add_channel ( self, channel ):
+        self[ 'channels' ].append( channel )    
+        
+    def add_environment ( self, environment ):
+        self[ 'environment' ].append( environment )
+
+    def add_holon( self, name ):
+        self[ 'holons' ].append( name )
